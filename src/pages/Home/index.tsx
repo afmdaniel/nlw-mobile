5e229ef7b07af774157826780e0055c2be34757d
afmdanielwin <danielfarias380@gmail.com>
2020-06-04 21:38:43 -0300

    Implement Home page layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { View, ImageBackground, Image, Text, StyleSheet } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
 
 const logo = require('../../assets/logo.png')
+const backgroundImage = require('../../assets/home-background.png')
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Image source={logo} />
-    </View>)
+    <ImageBackground 
+      source={backgroundImage}
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={logo} />
+        <Text style={styles.title}>Seu Marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente!</Text>
+      </View>
+      
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Feather name="arrow-right" color="#FFF" size={24} />
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
+      </View>
+    </ImageBackground>)
 }
 
 export default Home
@@ -16,6 +38,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
+    backgroundColor: '#F0F0F5'
   },
 
   main: {
